908e060b0dfd4dd3d61f8d4e1acd1a606367ffde
Matt Perpick <matthewperpick@gmail.com>
2010-04-03 18:10:58 -0400

    removing useless home controller, and instead redirecting to the event list page

--- radio/config/routes.rb
+++ radio/config/routes.rb
@@ -3,6 +3,8 @@ Radio::Application.routes.draw do |map|
   resources :artists
   resources :events
 
+  root :to => "events#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,9 +52,6 @@ Radio::Application.routes.draw do |map|
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
