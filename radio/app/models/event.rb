93f18f386e3373db111e259c704590f39d59a19d
Matt Perpick <matthewperpick@gmail.com>
2010-04-10 17:58:56 -0400

    adding validation/tests for event creation

--- radio/app/models/event.rb
+++ radio/app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :venue
   has_and_belongs_to_many :artists
+
+  validates_presence_of :venue_id, :start_date
 end
--- radio/test/unit/event_test.rb
+++ radio/test/unit/event_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
+
+  test "test create without venue" do
+    now = DateTime::now
+    event = Event.new(:start_date=>now)
+    assert !event.save, "shouldn't save event without venue"
+    assert event.errors.has_key? :venue_id
+  end
+
+  test "test create without start_date" do
+    now = DateTime::now
+    event = Event.new(:venue_id=>101)
+    assert !event.save, "shouldn't save event without start_date"
+    assert event.errors.has_key? :start_date
+  end
+
+
+  test "test ticket cost" do
     assert true
   end
 end
