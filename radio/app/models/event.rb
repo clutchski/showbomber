2afd47d042a1dacd60286aa5fc1bf408bf611256
Matt Perpick <matthewperpick@gmail.com>
2010-04-10 19:23:38 -0400

    added method/tests to get readable cost of a show

--- radio/app/models/event.rb
+++ radio/app/models/event.rb
@@ -13,4 +13,29 @@ class Event < ActiveRecord::Base
       errors.add(:max_cost, "The maximum cost is lower than the minimum.")
     end
   end
+
+  def to_dollars(n)
+    return "$#{n}"
+  end
+
+  def price_range_in_words
+
+    in_words = ''
+    if min_cost.blank? and max_cost.blank?
+      in_words = ''
+    elsif min_cost.blank? or max_cost.blank?
+      cost = min_cost.blank? ? max_cost : min_cost
+      in_words = to_dollars(cost)
+    elsif max_cost == 0 and min_cost == 0
+      in_words = 'free'
+    elsif max_cost == min_cost
+      in_words = to_dollars(min_cost)
+    else
+      min_dollars = to_dollars(min_cost)
+      max_dollars = to_dollars(max_cost)
+      in_words = "#{min_dollars} - #{max_dollars}"
+    end
+    in_words
+  end
+
 end
--- radio/test/unit/event_test.rb
+++ radio/test/unit/event_test.rb
@@ -24,4 +24,39 @@ class EventTest < ActiveSupport::TestCase
     assert event.errors.has_key? :max_cost
   end
 
+  test "test price range in words" do
+
+    # test empty range
+    event = Event.new
+    assert_equal "", event.price_range_in_words
+
+    # test only minimum cost
+    event.min_cost = 10
+    event.max_cost = nil
+    assert_equal "$10", event.price_range_in_words, "no max error"
+
+    # test only maximum cost
+    event.min_cost = nil
+    event.max_cost = 12
+    assert_equal "$12", event.price_range_in_words, "no min error"
+
+    # test free event
+    event.min_cost = 0
+    event.max_cost = 0
+    assert_equal 'free', event.price_range_in_words, "0 cost should be free"
+
+    # test identical max and min price
+    event.min_cost = 10
+    event.max_cost = 10
+    assert_equal "$10", event.price_range_in_words
+
+    # test normal range
+    event.min_cost = 10
+    event.max_cost = 20
+    assert_equal "$10 - $20", event.price_range_in_words
+
+
+
+  end
+
 end
