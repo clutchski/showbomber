3105407298d87551eecb68b179e2300e91b0a48d
Matt Perpick <matthewperpick@gmail.com>
2010-04-19 01:13:34 -0400

    a first pass a video, lots of owrk needed

--- radio/public/javascripts/radio.player.js
+++ radio/public/javascripts/radio.player.js
@@ -1,9 +1,37 @@
 
 radio.Player = Class.create({
 
-  initialize : function(container_selector, playlist) {
+  initialize : function(container_selector) {
+
+    this.DOM = radio.Player.DOM;
+
     this.container = $j(container_selector);
-    this.playlist = playlist;
+  },
+
+  playYouTubeVideo : function(url) {
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: 'myytplayer'};
+
+    var id = parseUri(url).queryKey.v;
+    var embedUrl = radio.Player.YOUTUBE_EMBED_URL.evaluate({'id':id});
+    swfobject.embedSWF(embedUrl, 
+    this.DOM.playerId, "425", "356", "8", null, null, params, atts);
+  },
+
+  play : function(songUrl) {
+    //FIXME: assert the url is for youtube
+    this.playYouTubeVideo(songUrl); 
   }
+
+});
+
+// Add class methods and varibles.
+Object.extend(radio.Player, {
+
+  DOM : { playerId : 'player'
+        },
+  YOUTUBE_EMBED_URL : new Template( 
+    "http://www.youtube.com/v/#{id}?enablejsapi=1&playerapiid=ytplayer")
+
 
 });
--- radio/public/javascripts/radio.playlist.js
+++ radio/public/javascripts/radio.playlist.js
@@ -1,13 +1,30 @@
 
 radio.Playlist = Class.create({
 
-  initialize : function(container_selector) {
+  initialize : function(container_selector, player) {
+    this.DOM = radio.Playlist.DOM;
+
+    this.player = player;
     this.container = $j(container_selector);
+
+    this.songLinks = this.container.find(this.DOM.songLinksClass);
+
+    this.songLinks.click(this.songClickHandler.bind(this));
+  }, 
+
+  songClickHandler : function(event) {
+    event.preventDefault();
+    var songLink = $j(event.target);
+    var songUrl = songLink.attr('href');
+    console.log('clicked ' + songUrl);
+    this.player.play(songUrl);
   }
 
 });
 
 // Add class methods and varibles.
 Object.extend(radio.Playlist, {
+
+  DOM : { songLinksClass : '.play_embed'}
 
 });
