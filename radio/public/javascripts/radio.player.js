68a337c9372a4c125d19873d10d41eb3e9f5eb2c
clutchski <clutchski@gmail.com>
2010-04-24 00:46:58 -0400

    refactoring player js/css

--- radio/public/javascripts/radio.player.js
+++ radio/public/javascripts/radio.player.js
@@ -1,36 +1,23 @@
-
 radio.Player = Class.create({
 
-  initialize : function(container_selector) {
+  initialize : function(videoId, playlistId) {
 
-    this.DOM = radio.Player.DOM;
+    this.videoContainer = $j("#" + videoId);
 
-    this.container = $j(container_selector);
-  },
-
-  playYouTubeVideo : function(url) {
-    var params = { allowScriptAccess: "always" };
-    var atts = { id: 'myytplayer'};
-
-    var id = parseUri(url).queryKey.v;
-    var embedUrl = radio.Player.YOUTUBE_EMBED_URL.evaluate({'id':id});
-    swfobject.embedSWF(embedUrl, 
-    this.DOM.playerId, "425", "356", "8", null, null, params, atts);
+    // initialize the playlist
+    var playSongCallback = this.play.bind(this);
+    this.playlist = new radio.Playlist(playlistId, playSongCallback);
   },
 
   play : function(songUrl) {
-    //FIXME: assert the url is for youtube
-    this.playYouTubeVideo(songUrl); 
+    console.log("player: " + songUrl);
   }
 
 });
 
-// Add class methods and varibles.
 Object.extend(radio.Player, {
 
-  DOM : { playerId : 'player'
-        },
-  YOUTUBE_EMBED_URL : new Template( 
+  EMBED_URL : new Template( 
     "http://www.youtube.com/v/#{id}?enablejsapi=1&playerapiid=ytplayer")
 
 
--- radio/public/javascripts/radio.playlist.js
+++ radio/public/javascripts/radio.playlist.js
@@ -1,14 +1,12 @@
-
 radio.Playlist = Class.create({
 
-  initialize : function(container_selector, player) {
+  initialize : function(playlistId, playSongCallback) {
     this.DOM = radio.Playlist.DOM;
 
-    this.player = player;
-    this.container = $j(container_selector);
+    this.container = $j('#' + playlistId);
+    this.playSongCallback = playSongCallback;
 
     this.songLinks = this.container.find(this.DOM.songLinksClass);
-
     this.songLinks.click(this.songClickHandler.bind(this));
   }, 
 
@@ -16,8 +14,8 @@ radio.Playlist = Class.create({
     event.preventDefault();
     var songLink = $j(event.target);
     var songUrl = songLink.attr('href');
-    console.log('clicked ' + songUrl);
-    this.player.play(songUrl);
+    console.log('passing song to player: ' + songUrl);
+    this.playSongCallback(songUrl);
   }
 
 });
