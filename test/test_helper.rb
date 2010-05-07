5e8fa551614def81491b4cdc4f7434a9377d7c29
Matt Perpick <matthewperpick@gmail.com>
2010-05-07 01:44:42 -0400

    added venue generation methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'rails/test_help'
+require 'forgery'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -9,5 +11,27 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+
+  #
+  # Test data creation methods
+  #
+
+  def new_venue(params={})
+    defaults = { :name        => "The #{Forgery::Name.company_name} Club",
+                 :address     => Forgery::Address.street_address,
+                 :city        => Forgery::Address.city,
+                 :state       => Forgery::Address.state,
+                 :postal_code => Forgery::Address.zip,
+                 :phone       => Forgery::Address.phone
+               }
+    defaults.merge(params)
+    Venue.new(defaults)
+  end
+
+  def create_venue(params={})
+    venue = new_venue(params)
+    venue.save
+    venue
+  end
+
 end
