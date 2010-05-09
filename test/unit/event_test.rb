2b87a6c778f85060c558b74c50b69944d1f2e699
Matt Perpick <matthewperpick@gmail.com>
2010-05-09 14:16:16 -0400

    cleaning up event tests
    * adding model generator
    * better names
    * less duplicated code

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -2,29 +2,21 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
 
-  test "test create without venue" do
-    now = DateTime::now
-    event = Event.new(:start_date=>now)
-    assert !event.save, "shouldn't save event without venue"
-    assert event.errors.has_key? :venue_id
+  test "events without required attributes are invalid" do
+    required_attributes = [:venue, :start_date]
+    required_attributes.each do |attribute|
+      event = EventGenerator.generate({attribute => nil})
+      assert event.invalid?, "events without #{attribute} are invalid"
+    end
   end
 
-  test "test create without start_date" do
-    now = DateTime::now
-    event = Event.new(:venue_id=>101)
-    assert !event.save, "shouldn't save event without start_date"
-    assert event.errors.has_key? :start_date
-  end
-
-  test "test create min cost is lower than max" do
-    now = DateTime::now
-    event = Event.new(
-      :venue_id=>101, :start_date=>now, :min_cost=>10, :max_cost=>8)
-    assert !event.save, "no save when max cost is less than min cost"
+  test "events with a greator maximum cost than minimum cost are invalid" do
+    event = EventGenerator.generate({:min_cost=>10, :max_cost=>8})
+    assert event.invalid?, "no save when max cost is less than min cost"
     assert event.errors.has_key? :max_cost
   end
 
-  test "test price range in words" do
+  test "events can give a readable version of their cost" do
 
     # test empty range
     event = Event.new
