a71dfcd1117eaf26f23ea02b7027ce9c934f2b7f
Matt Perpick <matthewperpick@gmail.com>
2010-05-09 22:03:01 -0400

    uncommented tests for event.by_artist

--- test/integration/models/event_test.rb
+++ test/integration/models/event_test.rb
@@ -21,22 +21,22 @@ class EventTest < ActiveSupport::TestCase
     events.each{ |e| assert_equal venue, e.venue }
   end
 
-  #test "events can be found with the scope by_artist" do
-  #  artist = ArtistGenerator.persisted
-  #  assert_not_nil artist.id
+  test "events can be found with the scope by_artist" do
+    artist = ArtistGenerator.persisted
+    assert_not_nil artist.id
 
-  #  assert Event.by_artist(artist).all().empty?
+    assert Event.by_artist(artist).all().empty?
 
-  #  num_events = 3
-  #  num_events.times.each do |i| 
-  #    EventGenerator.persisted({:artists => [artist]})
-  #  end
+    num_events = 3
+    num_events.times.each do |i| 
+      event = EventGenerator.persisted({:artists => [artist]})
+    end
 
-  #  puts Event.by_artist(artist).to_sql()
-  #  events = Event.by_artist(artist).all()
-  #  puts events
-  #  assert !events.empty?
-  #  assert_equal num_events, events.size
-  #  events.each do assert e.artists.include(artist) }
-  #end
+    events = Event.by_artist(artist).all()
+    assert !events.empty?
+    assert_equal num_events, events.size
+    events.each do |e| 
+      assert e.artists.include?(artist)
+    end
+  end
 end
