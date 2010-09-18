f3eb19c7ee1984baf4170b720fa5fc99a571879f
Matt Perpick <matthewperpick@gmail.com>
2010-09-18 01:10:17 -0400

    cleaning up youtube player class variables

--- public/javascripts/radio.youtube.js
+++ public/javascripts/radio.youtube.js
@@ -1,27 +1,12 @@
 radio.YouTube = {
-  
-  player : null,
-
-  state_by_id : $H({ '-1' : 'NOT_STARTED'
-                   , '0'  : 'ENDED'
-                   , '1'  : 'PLAYING'
-                   , '2'  : 'PAUSED'
-                   , '3'  : 'BUFFERING'
-                   , '5'  : 'CUED'
-                   }),
-
-  onPlayerStateChange : function(stateId) {
-    var state = radio.YouTube.state_by_id.get(stateId);
-    radio.YouTube.player.onPlayerStateChange(state);
-  }
+  singleton : null
 };
 
 radio.YouTube.Player = Class.create({
 
   initialize : function(containerId) {
 
-    // HACK: set the singleton to this instance
-    radio.YouTube.player = this;
+    radio.YouTube.singleton = this;
 
     this.cls = radio.YouTube.Player;
     this.containerId = containerId;
@@ -39,13 +24,13 @@ radio.YouTube.Player = Class.create({
 
   onPlayerReady : function() {
     this.player = document.getElementById(this.containerId);
-    var stateChangeCallback = "radio.YouTube.onPlayerStateChange";
+    var stateChangeCallback = "radio.YouTube.Player.onStateChange";
     this.player.addEventListener("onStateChange", stateChangeCallback);
     this.player.playVideo();
   },
 
-  onPlayerStateChange : function(state) {
-    if (state === 'ENDED') {
+  onStateChange : function(stateId) {
+    if (stateId === this.cls.states.ENDED) {
       this.log("video ended");
       $j('#' + this.containerId).trigger(this.cls.videoEnded);
     }
@@ -80,14 +65,24 @@ radio.YouTube.Player = Class.create({
 });
 
 Object.extend(radio.YouTube.Player, {
-  videoEnded : 'videoEnded'
+  videoEnded : 'videoEnded',
+
+  states : { NOT_STARTED : -1
+           , ENDED : 0
+           , PLAYING : 1
+           , PAUSED : 2
+           , BUFFERING : 3
+           , CUED : 5
+           },
+
+  onStateChange : function(stateId) {
+    radio.YouTube.singleton.onStateChange(stateId);
+  }
 });
 
 /* This function is called by the YouTube javascript API
  * when the player is ready.
  */
-var onYouTubePlayerReady = function(playerId) {
-  radio.YouTube.player.onPlayerReady();
+var onYouTubePlayerReady = function() {
+  radio.YouTube.singleton.onPlayerReady();
 };
-
-
