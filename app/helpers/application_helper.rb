e047a0cd2d0cba0c30caef3529db6e4e7ff767f5
Matt Perpick <matthewperpick@gmail.com>
2010-10-12 20:26:41 -0400

    Adding Google Analytics tracking.
    Fixes and closed case #10.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,22 @@ module ApplicationHelper
   def get_uservoice_url()
     return "http://showbomber.uservoice.com"
   end
+
+  def get_google_analytics_tag()
+    tag <<SCRIPT
+    <script type="text/javascript>
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-19073210-1']);
+      _gaq.push(['_trackPageview']);
+    
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    
+    </script>
+SCRIPT
+    return tag
+  end
 end
