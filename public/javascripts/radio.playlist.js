13509ed3774ca8804856de5c11cbd3e96aef90da
Matt Perpick <matthewperpick@gmail.com>
2010-10-14 00:33:14 -0400

    Added next and previous song buttons.
    Fixes and closes case #37.

--- public/javascripts/radio.playlist.js
+++ public/javascripts/radio.playlist.js
@@ -3,13 +3,18 @@ radio.Playlist = Class.create({
   onArtistSelectedEventName : 'artistSelected',
   artistClass : 'artist',
   nowPlayingClass : 'playing',
+  nextSongLinkId : 'next_song',
+  prevSongLinkId: 'previous_song',
 
   initialize : function(containerId) {
     this._container = $j('#' + containerId);
     this._artists = this._container.find('.artist');
-    this._artists.click(this._artistClickHandler.bind(this));
     this._fitPlaylistToWindow();
+    
+    this._artists.click(this._artistClickHandler.bind(this));
     $j(window).resize(this._fitPlaylistToWindow.bind(this));
+    var songNavSelector = '#' + this.nextSongLinkId + ',#' + this.prevSongLinkId;
+    $j(songNavSelector).click(this._songNavClickHandler.bind(this));
   },
 
   onArtistSelected: function(callback) {
@@ -18,6 +23,12 @@ radio.Playlist = Class.create({
 
   selectNextArtist : function() {
     var artist = this._getNextArtistLink();
+    this._selectArtistLink(artist);
+    return $j(artist).html();
+  },
+
+  selectPreviousArtist : function() {
+    var artist = this._getPreviousArtistLink();
     this._selectArtistLink(artist);
     return $j(artist).html();
   },
@@ -42,8 +53,34 @@ radio.Playlist = Class.create({
     this._selectArtistLink(artistLink);
   },
 
+  _songNavClickHandler : function(event) {
+    event.preventDefault();
+    var linkId = $j(event.target).attr('id');
+
+    var artist = (linkId === this.nextSongLinkId) ?
+                  this.selectNextArtist() : this.selectPreviousArtist();
+    if (artist) {
+      this._container.trigger(this.onArtistSelectedEventName, artist);
+    }
+  },
+
   _getCurrentArtistLink : function() {
     return this._artists.filter('.' + this.nowPlayingClass).first();
+  },
+
+  _getPreviousArtistLink : function() {
+    if (!this._artists) {
+      return null;
+    }
+
+    var curArtistLink = this._getCurrentArtistLink();
+    var curIndex = this._artists.index(curArtistLink);
+  
+    if (curIndex === null || curIndex <= 0) {
+      return null;
+    } else {
+      return this._artists.get(curIndex-1);
+    }
   },
 
   _getNextArtistLink : function() {
