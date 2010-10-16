4756ac99656b0e1f07109faad85d8b27e09e14c8
Matt Perpick <matthewperpick@gmail.com>
2010-10-16 03:44:59 -0400

    Added a view venue page.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,14 @@
 class EventsController < ApplicationController
 
   respond_to :html
-  layout "playlist"
 
   def index
     @events = Event.get_upcoming_events()
-    respond_with(@events)
+    render :layout => 'playlist'
   end
 
+  def show
+    @event = Event.where('id = ?', params[:id]).includes([:venue]).first()
+    respond_with(@event)
+  end
 end
