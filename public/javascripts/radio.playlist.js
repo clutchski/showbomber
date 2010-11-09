90ce55d1a0c9579e51ca146b345fd27c8cf8cb4c
Matt Perpick <matthewperpick@gmail.com>
2010-11-08 21:51:28 -0500

    Making main window smaller to avoid two scrollbars.

--- public/javascripts/radio.playlist.js
+++ public/javascripts/radio.playlist.js
@@ -35,7 +35,7 @@ radio.Playlist = Class.create({
 
   _fitPlaylistToWindow : function() {
     var windowHeight = $j(window).height();
-    var playlistHeight = windowHeight - 108;
+    var playlistHeight = windowHeight - 125;
     $j('#main').height(playlistHeight);
   },
 
