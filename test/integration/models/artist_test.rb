9458f38fb59f69b6eed2ea88f32bec7db539448d
Matt Perpick <matthewperpick@gmail.com>
2011-01-16 20:03:07 -0500

    Adding a test to ensure we can add songs to artists.

--- test/integration/models/artist_test.rb
+++ test/integration/models/artist_test.rb
@@ -7,4 +7,16 @@ class ArtistTest < ActiveSupport::TestCase
     artist = Factory.create(:artist, {:tags => tags})
     assert_equal tags.length, artist.tags.length
   end
+
+  test "Add song to artist" do
+    artist = Factory.create(:artist)
+
+    assert artist.songs.empty?
+    artist.songs.create({:url => 'http://foo.co'})
+
+    song = artist.songs(true).first
+    assert song
+    assert song.id
+    assert song.url = 'http://foo.co'
+  end
 end
